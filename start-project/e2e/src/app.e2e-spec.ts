d915fa393a959c2f141b8267d3b54e1496914bbc
William Henrique <william.henrique@live.com>
2019-07-11 13:19:38 -0300

    Subindo os fontes

--- start-project/e2e/src/app.e2e-spec.ts
+++ start-project/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to projeto!');
+    expect(page.getTitleText()).toEqual('Welcome to start-project!');
   });
 
   afterEach(async () => {
--- start-project/src/app/app.component.spec.ts
+++ start-project/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projeto'`, () => {
+  it(`should have as title 'start-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('projeto');
+    expect(app.title).toEqual('start-project');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to projeto!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to start-project!');
   });
 });
